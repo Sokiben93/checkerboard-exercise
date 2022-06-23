904840c0a316b8e4de2cd624be6609cac2dd5d4d
Thanh Huy Le <thanhhuyle@Thanhs-MBP.lan>
2022-06-23 18:20:18 -0500

    Update Checherboard test

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Your JS goes here
+var body = document.body;
+console.log(body);
+for (var i = 0; i < 63; i++) {
+  var div = document.createElement("div");
+  div.style.float = "left";
+  div.style.width = "11%";
+  div.style.paddingBottom = "11%";
+  if (i % 2 === 0) {
+    div.style.backgroundColor = "red";
+  } else {
+    div.style.backgroundColor = "black";
+  }
+  body.appendChild(div);
+}
